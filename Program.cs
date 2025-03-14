1c61d016b70f923cb7d7a6f4d94733fd412eb406
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 20:38:01 -0300

    Programa da Piada concluído.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿﻿Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("A Porta.");
+Thread.Sleep(1000);
+Console.WriteLine(@"Toc toc.
+- Quem é?
+O Prato.
+- Prato?
+Pratu ver como essa piada é ruim!");
